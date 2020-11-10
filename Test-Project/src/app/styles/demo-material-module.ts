cfef809dd99d34d83330e8310793c3ec8b299984
Yifan-Song0819 <yfsong19@gmail.com>
2020-11-11 10:44:16 +1300

    dropdown

--- Test-Project/src/app/styles/demo-material-module.ts
+++ Test-Project/src/app/styles/demo-material-module.ts
@@ -4,19 +4,22 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   imports: [
     MatButtonModule,
     MatCardModule,
     MatGridListModule,
-    MatMenuModule
+    MatMenuModule,
+    MatExpansionModule
   ],
   exports: [
     MatButtonModule,
     MatCardModule,
     MatGridListModule,
-    MatMenuModule
+    MatMenuModule,
+    MatExpansionModule
   ]
 })
 export class DemoMaterialModule {}
